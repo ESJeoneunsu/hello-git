988d46b1391499a44cd03c5a4b9f6c2c76a7d6d9
ES <victoryt@naver.com>
2018-05-30 20:58:41 +0900

    Fourth commit

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
   public static void main(String[] args){
     System.out.println("Hello, world form GIT!");
     Systme.out.println("Changes after first commit");
+    System.out.println("Chanes after Pushing to remote!");
   }
 }
